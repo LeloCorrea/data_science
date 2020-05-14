6ef8eac13db91168e637659bb1f1c6efc583dfd1
Anderson Cunha <andersoncorreadacunha@gmail.com>
2020-05-13 23:25:23 -0300

    Aula 01

--- Aula01.py
+++ Aula01.py
@@ -1,5 +1,26 @@
 '''
 Desafio: Previsão do valor do imóvel.
 Objetivo: Prever o valor de imóveis de uma determinada cidade através de uma tecnologia.
+'''
 
 '''
+Esses comandos na verdade não são exclusivos do colaboratory, os mesmos comandos funcionam no Jupyter notebook local, no vscode, no Pycharm.
+'''
+# 1 - Importando os módulos para (Leitura de um arquivo xlsx):
+import pandas as pd
+import numpy as np
+
+# 2 - Lendo os arquivos excel:
+#df = pd.read_excel(' caminho do arquivo excel ', index_col=None, header=None)
+
+# 3 - Visualizando os dados:
+# df.head()
+
+# ---
+
+# Semana de Ciência de Dados
+# Minerando Dados
+
+# Conhecendo a base de dados
+
+# Monta o drive
